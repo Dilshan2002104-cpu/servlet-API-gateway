d6981e3419010616b0c1604f67c411b8d9fd44ea
Dilshan2002104-cpu <Praveendilshanr@gmail.com>
2026-04-13 12:48:27 +0530

    Add project source files and configuration

---
+++ src/main/java/com/gateway/AuthFilter.java
@@ -0,0 +1,34 @@
+package com.gateway;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class AuthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        System.out.println("Request reserved: " + httpRequest.getRequestURI());
+
+        chain.doFilter(request, response);
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
---
+++ src/main/java/com/gateway/RouterServlet.java
@@ -0,0 +1,20 @@
+package com.gateway;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/api/*")
+public class RouterServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        response.setContentType("application/json");
+        response.getWriter().write("{\"message\": \"Gateway is working!\"}");
+    }
+}
